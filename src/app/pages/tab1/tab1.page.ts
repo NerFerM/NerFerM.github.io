efbd3731957aff9319bc7d1df4b3a9eca6d65a75
Nerea <nerea.fernandez@tcomet.com>
2022-11-02 13:20:42 +0100

    modified:   src/app/app.module.ts 	modified:   src/app/pages/tab1/tab1.page.ts 	new file:   src/app/services/posts.service.ts 	modified:   src/environments/environment.prod.ts 	modified:   src/environments/environment.ts

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private postsService: PostsService) { }
+
+  ngOnInit() {
+    this.postsService.getPosts().subscribe(resp => {
+      console.log(resp);
+    })
+  }
 
 }
