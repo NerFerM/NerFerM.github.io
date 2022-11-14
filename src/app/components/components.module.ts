7d315e4cea640143b017cce7bcdd906b2ccab923
Nerea <nerea.fernandez@tcomet.com>
2022-11-14 15:31:22 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/components/components.module.ts 	modified:   src/app/components/post/post.component.html 	modified:   src/app/components/post/post.component.ts

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,6 +5,7 @@ import { PostComponent } from './post/post.component';
 import { IonicModule } from '@ionic/angular';
 import { PipesModule } from '../pipes/pipes.module'
 import { AvatarSelectorComponent } from './avatar-selector/avatar-selector.component';
+import { VideoPlayer } from '@awesome-cordova-plugins/video-player/ngx';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,9 @@ import { AvatarSelectorComponent } from './avatar-selector/avatar-selector.compo
     CommonModule, 
     IonicModule,
     PipesModule
+  ],
+  providers: [
+    VideoPlayer
   ]
 })
 export class ComponentsModule {}
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Post } from 'src/app/interfaces/interfaces';
+import { VideoPlayer } from '@awesome-cordova-plugins/video-player/ngx';
+declare var HKVideoPlayer;
 
 @Component({
   selector: 'app-post',
@@ -14,7 +16,15 @@ export class PostComponent implements OnInit {
     allowSlidePrev: false
   }
 
-  constructor() {}
+  constructor(private videoPlayer: VideoPlayer) {}
 
   ngOnInit() {}
+
+  play() {
+    this.videoPlayer.play("{{post.video}}").then(() => {
+      console.log('Cargando vídeo');
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 }
